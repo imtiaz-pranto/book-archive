f76732eb656e936c0f658cc05c18fa959a75d7e5
Imtiaz Pranto <47033546+imtidev@users.noreply.github.com>
2021-09-02 02:36:50 +0600

    Search Added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const searchBook = () => {
   //clear data
   searchField.value = "";
 
-  if (searchText == "") {
+  if (searchText === "") {
     //display error message
     const searchResult = document.getElementById("search-result");
     const div = document.createElement("div");
@@ -22,16 +22,17 @@ const searchBook = () => {
     //load data
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
     console.log(url);
-
     fetch(url)
       .then((res) => res.json())
-      .then((data) => displaySearchResult(data.books));
+      .then((data) => displaySearchResult(data));
   }
 };
-const displaySearchResult = (books) => {
+const displaySearchResult = (data) => {
+  const books = data.docs;
+  //   console.log(books);
   const searchResult = document.getElementById("search-result");
   searchResult.textContent = "";
-  if (books.length == 0) {
+  if (books.length === 0) {
     //show no result found
     const div = document.createElement("div");
     div.classList.add("col");
@@ -43,18 +44,20 @@ const displaySearchResult = (books) => {
     `;
     searchResult.appendChild(div);
   } else {
-    console.log(searchResult);
+    // console.log(searchResult);
     books.forEach((book) => {
       console.log(book);
       const div = document.createElement("div");
       div.classList.add("col");
       div.innerHTML = `
-        <div onclick="loadbookDetail(${book.idbook})" class="card h-100 mb-3">
-          <img src="${book.strbookThumb}" class="card-img-top" alt="..." />
+        <div class="card h-100 mb-3">
           <div class="card-body">
-            <h5 class="card-title">${book.strbook}</h5>
+            <h5 class="card-title">${book.title}</h5>
             <p class="card-text">
-              ${book.strInstructions.slice(0, 200)}
+            Author:  ${book.author_name}
+            </p>
+            <p class="card-text">
+             Pulish Date : ${book.first_publish_year}
             </p>
           </div>
         </div>
@@ -62,33 +65,4 @@ const displaySearchResult = (books) => {
       searchResult.appendChild(div);
     });
   }
-};
-
-const loadbookDetail = (bookId) => {
-  console.log(bookId);
-  const url = `https://www.thebookdb.com/api/json/v1/1/lookup.php?i=${bookId}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displaybookDetail(data.books[0]));
-};
-
-const displaybookDetail = (book) => {
-  console.log(book);
-  const bookDetails = document.getElementById("book-details");
-  bookDetails.textContent = "";
-  const div = document.createElement("div");
-  div.classList.add("card");
-  div.innerHTML = `
-      <div class="card">
-        <img src="${book.strbookThumb}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${book.strbook}</h5>
-          <p class="card-text">${book.strInstructions.slice(0, 150)}</p>
-          <a href="${
-            book.strYoutube
-          }" class="btn btn-primary w-25 mx-auto">Visit Youtube</a>
-        </div>
-      </div>
-`;
-  bookDetails.appendChild(div);
 };
