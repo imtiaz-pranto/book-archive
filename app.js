b400a995dd0fc688e38cd24b8b4cc51cc6a8d3f1
Imtiaz Pranto <47033546+imtidev@users.noreply.github.com>
2021-09-02 12:30:50 +0600

    search result amount added

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const displaySearchResult = (data) => {
   if (books.length === 0) {
     //show no result found
     const div = document.createElement("div");
-    div.classList.add("col");
+    // div.classList.add("col");
     div.innerHTML = `
         <div class="card h-100 mb-3 bg-danger">
           <h3>No Result Found! Please Try Another Keyword! </h3>
@@ -44,7 +44,18 @@ const displaySearchResult = (data) => {
     `;
     searchResult.appendChild(div);
   } else {
-    // console.log(searchResult);
+    // Number of Search Result
+    const resultAmount = document.getElementById("result-amount");
+    resultAmount.textContent = "";
+    const p = document.createElement("p");
+    p.classList.add("font-weight-bold");
+    p.innerHTML = `
+      <p>Total ${data.numFound} Results Found! </p>
+      `;
+    resultAmount.appendChild(p);
+    //
+
+    console.log(searchResult);
     books.forEach((book) => {
       console.log(book);
       const div = document.createElement("div");
@@ -52,16 +63,22 @@ const displaySearchResult = (data) => {
       div.innerHTML = `
         <div class="card h-100 mb-3">
           <div class="card-body">
+           <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt=""></img>
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text">
-            Author:  ${book.author_name}
+            Author: ${book.author_name}
             </p>
             <p class="card-text">
              Pulish Date : ${book.first_publish_year}
             </p>
+            <p class="card-text">
+             Pulisher : ${book.publisher}
+            </p>
           </div>
         </div>
     `;
+      // }
+
       searchResult.appendChild(div);
     });
   }
