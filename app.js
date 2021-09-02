31d0faaae0359539ccb2644b7dc89001cdea6f4f
Imtiaz Pranto <47033546+imtidev@users.noreply.github.com>
2021-09-02 19:33:51 +0600

    fixed bugs

--- app.js
+++ app.js
@@ -3,42 +3,44 @@ const toggleSpinner = (displaySpinner) => {
   document.getElementById("spinner").style.display = displaySpinner;
 };
 
+//Cleaing all textcontents
+const clearElements = () => {
+  const searchResult = document.getElementById("search-result");
+  searchResult.textContent = "";
+  const resultAmount = document.getElementById("result-amount");
+  resultAmount.textContent = "";
+};
+
 const searchResult = document.getElementById("search-result");
+const resultAmount = document.getElementById("result-amount");
 
 const searchBook = () => {
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   console.log(searchText);
-  toggleSpinner("block"); //spinner visible after clicking search
-
-  //clear searchbox
   searchField.value = "";
+  toggleSpinner("block"); //spinner visible after clicking search
 
   //check if search keyword is inserted
   if (searchText === "") {
     //display error message
-    const searchResult = document.getElementById("search-result");
-    searchResult.textContent = "";
-    const resultAmount = document.getElementById("result-amount");
-    resultAmount.textContent = "";
+    clearElements();
     const div = document.createElement("div");
-    div.classList.add("col");
     div.innerHTML = `
-        <div class="card h-100 mb-3 bg-danger text-white">
-          <h3>Please Enter a Keyword! </h3>
+        <div class="h-100 p-2 mb-3 text-warning">
+          <h5>Please Enter a Keyword! </h5>
           </div>
         </div>
     `;
-    //setting spinner invisible and showing warning
     toggleSpinner("none");
-    searchResult.appendChild(div);
+    resultAmount.appendChild(div);
   }
-  //results are found
+
+  //results are fetched
   else {
     //load data from api
-    const searchResult = document.getElementById("search-result");
-    searchResult.textContent = "";
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    clearElements();
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     console.log(url);
     fetch(url)
       .then((res) => res.json())
@@ -49,44 +51,40 @@ const searchBook = () => {
 //displaying fetched data
 const displaySearchResult = (data) => {
   const books = data.docs;
-  //   console.log(books);
-  searchResult.textContent = "";
+  clearElements();
   if (books.length === 0) {
-    searchResult.textContent = "";
-    const resultAmount = document.getElementById("result-amount");
-    resultAmount.textContent = "";
-    //show no result found
     const div = document.createElement("div");
-    // div.classList.add("col");
     div.innerHTML = `
-        <div class="card h-100 mb-3 bg-danger">
+        <div class="h-100 p-2 mb-3 text-danger">
           <h3>No Result Found! Please Try Another Keyword! </h3>
           </div>
         </div>
     `;
     toggleSpinner("none");
-    searchResult.appendChild(div);
-  } else {
+    resultAmount.appendChild(div);
+  }
+
+  //When results are found
+  else {
     // Number of Search Result
-    const resultAmount = document.getElementById("result-amount");
-    resultAmount.textContent = "";
     const p = document.createElement("p");
-    p.classList.add("font-weight-bold");
     p.innerHTML = `
-      <p>Total ${data.numFound} Results Found! </p>
+      <h4>Total ${data.numFound} Results Found! </h4>
       `;
     resultAmount.appendChild(p);
-    //
 
-    console.log(searchResult);
+    //Show Books
     books.forEach((book) => {
       console.log(book);
       const div = document.createElement("div");
       div.classList.add("col");
+
       div.innerHTML = `
         <div class="card h-100 mb-3">
           <div class="card-body">
-           <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt=""></img>
+           <img class="img-fixed" src="https://covers.openlibrary.org/b/id/${
+             book.cover_i ? book.cover_i : 10909258
+           }-M.jpg" alt=""></img>
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text">
             Author: ${book.author_name}
@@ -95,7 +93,7 @@ const displaySearchResult = (data) => {
              Pulish Date : ${book.first_publish_year}
             </p>
             <p class="card-text">
-             Pulisher : ${book.publisher}
+             Publisher : ${book.publisher}
             </p>
           </div>
         </div>
